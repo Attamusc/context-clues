b5ccf7be31ed0b6e7e7a6fdfa01d10e113628b59
Sean Dunn <sean@attamusc.com>
2012-02-07 00:31:21 -0500

    Cool styles and a placeholder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,14 @@
 /* Author: 
-
+    Sean Dunn
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+(function($) {
+    $("input").on("keypress", function(event) {
+        if(event.keyCode === 13) {
+            // For now, just unhide the div
+            // Eventually, make a websocket call to the server
+            this.value = "";
+            $("#bayes").fadeIn("normal");
+        }
+    });
+})(jQuery);
